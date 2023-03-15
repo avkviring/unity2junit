3e29d1c978eea49213951414b97c13e806a174a7
Kviring Aleksey <alex@kviring.com>
2023-03-15 16:39:00 +0500

    Адаптация под новый формат test.xml

--- src/unity.rs
+++ src/unity.rs
@@ -3,76 +3,30 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TestResults {
     #[serde(rename = "test-suite")]
-    pub test_suite: Option<TestSuite>,
+    pub test_suite: Option<Vec<TestSuite>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TestSuite {
     pub r#type: Option<String>,
-    pub name: Option<String>,
-    pub executed: Option<String>,
-    pub result: Option<String>,
-    pub success: Option<String>,
-    pub asserts: Option<u64>,
-    pub results: Option<Results>,
-    pub reason: Option<Reason>,
-}
-#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
-pub struct Results {
-    #[serde(rename = "$value")]
-    pub items: Vec<SuiteOrCase>,
-}
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub enum SuiteOrCase {
+    pub fullname: String,
+    pub result: String,
+    pub total: u64,
+    pub passed: u64,
+    pub skipped: u64,
     #[serde(rename = "test-suite")]
-    TestSuite(TestSuite),
+    pub test_suite: Option<Vec<TestSuite>>,
     #[serde(rename = "test-case")]
-    TestCase(TestCase),
+    pub test_case: Option<Vec<TestCase>>,
 }
 
+//   <test-case id="1127" name="AttackRequestTest" fullname="DMP.Battle.Client.Test.CombatActions.CombatActionSystemTest.AttackRequestTest" methodname="AttackRequestTest" classname="DMP.Battle.Client.Test.CombatActions.CombatActionSystemTest" runstate="Runnable" seed="1588844276" result="Passed" start-time="2023-03-15 11:11:59Z" end-time="2023-03-15 11:11:59Z" duration="0.132441" asserts="0">
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TestCase {
-    pub name: Option<String>,
-    pub executed: Option<String>,
-    pub result: Option<String>,
-    pub success: Option<String>,
-    pub time: Option<String>,
-    pub asserts: Option<u64>,
-    pub failure: Option<Failure>,
-    pub reason: Option<Reason>,
-    pub properties: Option<Properties>,
-    pub categories: Option<Categories>,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Properties {
-    #[serde(rename = "$value")]
-    pub items: Vec<Property>,
-}
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Categories {
-    #[serde(rename = "$value")]
-    pub items: Vec<Category>,
-}
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Property {
     pub name: String,
-    pub value: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Category {
-    pub name: String,
-}
-
-#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
-pub struct Failure {
-    pub message: Option<String>,
-    #[serde(rename = "stack-trace")]
-    pub stack_trace: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Reason {
-    pub message: Option<String>,
+    pub classname: String,
+    pub methodname: String,
+    pub fullname: String,
+    pub result: String,
+    pub output: Option<String>,
 }
